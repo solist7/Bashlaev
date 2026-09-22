37f46b732f7692d6317f2a56b42ccce691dc0958
solist7 <aizensosuke694@gmail.com>
2026-09-22 21:16:28 +0000

    2 задание

--- laba3.py
+++ laba3.py
@@ -1,9 +1,9 @@
 from math import*
 a = 0.1
-b = 1,2
-n = int(round(b-a)/step)
+b = 1.2
 step = 0.05
+n = int(round((b-a)/step)) + 1
 for i in range(n):
   x = a+i*step
-  y = pow(3,asin(x/2)) + log(3,2*x)
-  print(y)
+  y = 3**(asin(x/2)) + log(x*2,3)
+  print(y,x)
